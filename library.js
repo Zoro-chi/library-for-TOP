42426628929037ff174c21f624e13fcab38db719
zoro <jagbetuyi001@gmail.com>
2022-04-05 01:17:14 +0100

    sorting item cards

--- library.js
+++ library.js
@@ -1,12 +1,12 @@
 
-let item = document.querySelector(".item-card").innerText
 
+//let item = document.querySelector(".item-card").innerText
 
 const book1 = {
-    Author : "Mr.Me",
-    Title : "GOT",
+    Author : "Mr.Mike Myers",
+    Title : "Game of Thrones",
     Pages : 300,
-    Read : true,
+    Read : "Yes",
 }
 
 let myLib = [book1]
@@ -21,3 +21,8 @@ function Book(author, title, numberOfPages, read) {
 function addBookToLib(userInput) {
     myLib.push(userInput)
 }
+
+document.querySelector(".author").innerHTML = `Author : ${book1.Author}`
+document.querySelector(".title").innerHTML = `Title : ${book1.Title}`
+document.querySelector(".pages").innerHTML = `Pages : ${book1.Pages}`
+document.querySelector(".read").innerHTML = `Read? : ${book1.Read}`
